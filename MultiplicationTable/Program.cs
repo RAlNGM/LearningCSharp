72a86bef6a878907e992392eb85dd39eac4cf63b
RAINGM <danya.fominix@yandex.ru>
2022-12-15 14:23:34 +0400

    to table

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -2,13 +2,24 @@
 {
     static void Main(string[] args)
     {
-        for (int i = 1; i <= 10; i++)
+        /*for (int i = 1; i <= 10; i++)
         {
             for (int j = 1; j <= 10; j++)
             {
                 Console.Write("{0} X {1} = {2}\n", i, j, i * j);
             }
             Console.WriteLine("\n");
+        }*/
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.Write(i + "\t");
+            for (int j = 1; j <= 10; j++)
+            {
+                if (i > 0) { Console.Write(i * j + "\t"); }
+                else { Console.Write(j + "\t"); }
+            }
+            Console.Write("\n");
         }
     }
 }
