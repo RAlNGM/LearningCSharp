c19f6e84714423e9179cc7b789b0321c88a16097
RAINGM <danya.fominix@yandex.ru>
2022-12-15 16:41:52 +0400

    new unit test

--- UnitTests/MyPrimeNumberLib.Tests/UnitTest1.cs
+++ UnitTests/MyPrimeNumberLib.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace MyPrimeNumberLib.Tests
 {
     [TestClass]
@@ -14,5 +16,28 @@ namespace MyPrimeNumberLib.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void is_leap_year_test()
+        {
+            List<bool> excepted_ = new List<bool>()
+            {
+                true, false, false, false, true, false, false
+            };
+            List<int> years = new List<int>()
+            { 
+                2016, 2017, 2018, 2019, 2020, 2021, 2022
+            };
+
+            LeapYear year_is_leap = new LeapYear();
+            for (int i = 0; i < years.Count; i++)
+            {
+                bool actual = year_is_leap.isLeapYear(years[i]);
+                Console.WriteLine($"{years[i]} {excepted_[i]} === {actual}");
+                Assert.AreEqual(excepted_[i], actual);
+            }
+        }
+
+
     }
 }
